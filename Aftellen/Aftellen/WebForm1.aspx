94a5b2df1c214055ef23484c085811c13e82f5fb
iceyelf1 <josha-wesselink@hotmail.com>
2017-03-20 15:08:59 +0100

    Opdracht4

---
+++ Aftellen/Aftellen/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Aftellen.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="StyleSheet1.css">
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Het Aftellen is begonnen</h1>
+        Het is vandaag:<br />
+        <asp:Label ID="litTijd" runat="server" Text="litTijd"></asp:Label>
+        <br />
+        <br />
+        <br />
+        het is nu:<br />
+        <asp:Label ID="litVandaag" runat="server" Text="litVandaag"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <h2>Wanneer is het Pasen?</h2>
+        <p>&nbsp;</p>
+        <p>Het duurt nog: </p>
+        <p>
+            <asp:Label ID="lblTotaal" runat="server" Text="Totaal "></asp:Label>
+        </p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>
+            <asp:Button ID="Refresh" runat="server" OnClick="Button1_Click" Text="btnRefresh" />
+        </p>
+
+    </form>
+</body>
+</html>
---
+++ Aftellen/Aftellen/WebForm1.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aftellen
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DateTime Nu = DateTime.Now;
+            string NuDatum = "De datum van vandaag is: " + Nu;
+
+            DateTime Pasen = new DateTime(2017, 4, 16);
+            string PasenDatum = "Pasen valt op: " + Pasen;
+
+            TimeSpan EindeTijd = Pasen.Subtract(Nu);
+
+            int Dagen = EindeTijd.Days;
+            int Uren = EindeTijd.Hours;
+            int Minuten = EindeTijd.Minutes;
+            int Seconden = EindeTijd.Seconds;
+
+            string Total =
+                "Dagen: " + Dagen + "<br>" +
+                "Uren: " + Uren + "<br>" +
+                "Minuten: " + Minuten + "<br>" +
+                "Seconden: " + Seconden + "<br>";
+
+            lblTotaal.Text = Total;
+            litTijd.Text = PasenDatum;
+            litVandaag.Text = NuDatum;
+
+        }
+    }
+}
